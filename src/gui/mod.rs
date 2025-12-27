98393a8bca25871cdf6dd13c609acc76fafeef08
ken <39673849+SuperKenVery@users.noreply.github.com>
2025-12-27 15:56:15 +0800

    Remove debug code

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,10 +1,7 @@
 pub mod app;
 mod steps;
 pub mod utils;
-use dioxus::prelude::dioxus_fullstack;
 
 pub fn start_app() {
-    let server_path = dioxus_fullstack::get_server_url();
-    tracing::info!("Server path: {}", server_path);
     dioxus::launch(app::App);
 }
